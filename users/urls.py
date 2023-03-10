32f7d0313d2443d53e5b48e8993fc6d4b8f7105f
Uni App <126077396+cpinnockuni@users.noreply.github.com>
2023-03-10 11:56:29 +0000

    update

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path, include
 from .views import home, profile, RegisterView
 from . import views 
+
+
+
+
+
 urlpatterns = [
     path('', home, name='users-home'),
     path('register/', RegisterView.as_view(), name='users-register'),
     path('profile/', profile, name='users-profile'),
     path('com/', views.com, name="community"),
 ]
+
+
+
